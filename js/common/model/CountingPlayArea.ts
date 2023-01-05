46f83ac709eaaafdcccbaae65c64770511ff5815
phet-dev <phetadmin@gmail.com>
2023-01-05 04:48:50 -0700

    update copyright dates from daily grunt work

--- js/common/model/CountingPlayArea.ts
+++ js/common/model/CountingPlayArea.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * Model for the Explore screen in Make a Ten. Includes the total, cues, and adding in initial numbers. This file was
--- js/common/view/CountingAccordionBox.ts
+++ js/common/view/CountingAccordionBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * Class for the 'Objects' accordion box, which is the panel in the lower right corner of the sim that displays an
