fe4fe7ac30d9e3761282ca6ed44e6c99d0615b17
pixelzoom <cmalley@pixelzoom.com>
2023-01-09 17:15:41 -0700

    workaround for https://github.com/phetsims/number-play/issues/172

--- js/common/model/CountingPlayArea.ts
+++ js/common/model/CountingPlayArea.ts
@@ -400,8 +400,9 @@ class CountingPlayArea extends CountingCommonModel {
         const numberOfRows = NumberSuiteCommonConstants.TEN;
 
         const origin = stack ? countingObjectPosition.minusXY( 0, 25 ) : countingObjectPosition;
-        const offsetYSegment = stack ? ( countingObject.localBounds.height - CountingCommonConstants.PLAY_OBJECT_SIZE.height ) /
-                                       ( numberOfRows + 1 ) : 0;
+        const offsetYSegment = stack ? CountingCommonConstants.BREAK_APART_Y_OFFSET : 0;
+        console.log( `offsetYSegment=${offsetYSegment}` );//XXX
+
         let offsetY = 0;
 
         let reAddedCountingObjects = 0;
