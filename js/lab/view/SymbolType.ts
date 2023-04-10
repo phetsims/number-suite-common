f518ada9826020359ddc428cda3c598a50bfb76e
chrisklus <christopher.klusendorf@colorado.edu>
2023-04-10 12:44:26 -0600

    Use MathSymbols instead of characters, use SymbolType file instead of inline export, see https://github.com/phetsims/number-play/issues/225

--- js/lab/view/SymbolType.ts
+++ js/lab/view/SymbolType.ts
@@ -1,10 +1,13 @@
 // Copyright 2023, University of Colorado Boulder
 
 /**
- * Allowed symbols
+ * Allowed symbols for cards on the 'Lab' screen.
  *
  * @author Chris Klusendorf (PhET Interactive Simulations)
  */
 
-type SymbolType = '<' | '=' | '>' | '+' | '-';
+import MathSymbols from '../../../../scenery-phet/js/MathSymbols.js';
+
+type SymbolType = typeof MathSymbols.LESS_THAN | typeof MathSymbols.EQUAL_TO | typeof MathSymbols.GREATER_THAN |
+  typeof MathSymbols.PLUS | typeof MathSymbols.MINUS;
 export default SymbolType;
