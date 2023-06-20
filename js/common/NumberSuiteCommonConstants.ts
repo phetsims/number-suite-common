0cde4d98d4f58014be7222f6d82385f7faa83fda
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-20 12:16:27 -0600

    remove as many LinkableElement usages as possible, using PhetioProperty where we have to, https://github.com/phetsims/tandem/issues/299
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/common/NumberSuiteCommonConstants.ts
+++ js/common/NumberSuiteCommonConstants.ts
@@ -6,7 +6,6 @@
  * @author Chris Klusendorf (PhET Interactive Simulations)
  */
 
-import LinkableProperty from '../../../axon/js/LinkableProperty.js';
 import PhetFont from '../../../scenery-phet/js/PhetFont.js';
 import numberSuiteCommon from '../numberSuiteCommon.js';
 import NumberSuiteCommonStrings from '../NumberSuiteCommonStrings.js';
@@ -14,9 +13,10 @@ import { SecondLocaleStrings } from './model/NumberSuiteCommonPreferences.js';
 import SceneryPhetConstants from '../../../scenery-phet/js/SceneryPhetConstants.js';
 import CountingCommonConstants from '../../../counting-common/js/common/CountingCommonConstants.js';
 import TReadOnlyProperty from '../../../axon/js/TReadOnlyProperty.js';
+import PhetioProperty from '../../../axon/js/PhetioProperty.js';
 
 // Maps a number to the stringProperty of the translated word that corresponds to the number.
-const NUMBER_TO_STRING_PROPERTY_PRIMARY: Record<number, LinkableProperty<string>> = {
+const NUMBER_TO_STRING_PROPERTY_PRIMARY: Record<number, PhetioProperty<string>> = {
   0: NumberSuiteCommonStrings.zeroStringProperty,
   1: NumberSuiteCommonStrings.oneStringProperty,
   2: NumberSuiteCommonStrings.twoStringProperty,
