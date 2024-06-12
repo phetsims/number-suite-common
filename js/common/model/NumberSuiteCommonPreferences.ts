3a52664bb2d96b6b088284b3071a86dd05fbde5b
phet-dev <phetadmin@gmail.com>
2024-06-12 03:17:29 -0600

    update copyright dates from daily grunt work

--- js/common/model/NumberSuiteCommonPreferences.ts
+++ js/common/model/NumberSuiteCommonPreferences.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * NumberSuiteCommonPreferences is the model for sim-specific preferences for all Number Suite sims, accessed via the
