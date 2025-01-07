b324b6642d208ded503bb088e8d5d8e282b0985a
phet-dev <phetadmin@gmail.com>
2025-01-07 03:30:47 -0700

    update copyright dates from daily grunt work

--- js/common/view/LanguageAndVoiceControl.ts
+++ js/common/view/LanguageAndVoiceControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2023-2025, University of Colorado Boulder
 
 /**
  * LanguageAndVoiceControl contains two carousels: one for selecting a language, and one for selecting a voice. For use
