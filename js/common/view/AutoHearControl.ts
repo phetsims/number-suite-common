805d24c984126e983672d1458fdc41db70f9b01c
phet-dev <phetadmin@gmail.com>
2025-01-28 03:41:13 -0700

    update copyright dates from daily grunt work

--- js/common/view/AutoHearControl.ts
+++ js/common/view/AutoHearControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2022-2025, University of Colorado Boulder
 
 /**
  * A toggle control in the Preferences Dialog that controls whether the sim automatically reads the current data out
