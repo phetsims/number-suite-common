bdf2c30cc60ce5f4840de6c31aa0ecf4d70e3f0f
Marla Schulz <marla.schulz@colorado.edu>
2025-01-28 13:25:20 -0800

    Comment out dynamic max width, see: https://github.com/phetsims/number-pairs/issues/67

--- js/common/view/LocaleSwitch.ts
+++ js/common/view/LocaleSwitch.ts
@@ -13,7 +13,7 @@ import NumberSuiteCommonPreferences from '../../../../number-suite-common/js/com
 import NumberSuiteCommonUtteranceQueue from '../../../../number-suite-common/js/common/view/NumberSuiteCommonUtteranceQueue.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import { Text } from '../../../../scenery/js/imports.js';
+import { Text, TextOptions } from '../../../../scenery/js/imports.js';
 import ABSwitch, { ABSwitchOptions } from '../../../../sun/js/ABSwitch.js';
 import numberSuiteCommon from '../../numberSuiteCommon.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
@@ -24,7 +24,7 @@ type LocaleSwitchOptions = SelfOptions & ABSwitchOptions;
 // constants
 const TOGGLE_SWITCH_SIZE = new Dimension2( 40, 20 );
 const SPACING = 8;
-const TEXT_OPTIONS = {
+const TEXT_OPTIONS: TextOptions = {
   font: new PhetFont( 14 )
 };
 
@@ -42,6 +42,7 @@ export default class LocaleSwitch extends ABSwitch<boolean> {
     const secondLanguageStringProperty = new DerivedProperty( [ preferences.secondLocaleProperty ],
       StringUtils.localeToLocalizedName );
 
+    TEXT_OPTIONS.maxWidth = ( maxWidth - TOGGLE_SWITCH_SIZE.width - SPACING * 2 ) / 2;
     const firstLanguageText = new Text( firstLanguageStringProperty, TEXT_OPTIONS );
     const secondLanguageText = new Text( secondLanguageStringProperty, TEXT_OPTIONS );
 
@@ -61,24 +62,24 @@ export default class LocaleSwitch extends ABSwitch<boolean> {
     // Speak speechData if autoHear is turned on.
     this.onInputEmitter.addListener( () => preferences.autoHearEnabledProperty.value && utteranceQueue.speakSpeechData() );
 
-    const availableTextSpace = maxWidth - TOGGLE_SWITCH_SIZE.width - SPACING * 2;
-    let isAdjusting = false; // to prevent recursion that will exceed maximum call stack size
-    this.boundsProperty.link( () => {
-      if ( !isAdjusting ) {
-        isAdjusting = true;
-
-        // Assume that neither label needs to be scaled.
-        firstLanguageText.maxWidth = null;
-        secondLanguageText.maxWidth = null;
-
-        // If there's not enough space for both full-size labels, give each Text label half of the available space.
-        if ( firstLanguageText.width + secondLanguageText.width > availableTextSpace ) {
-          firstLanguageText.maxWidth = availableTextSpace / 2;
-          secondLanguageText.maxWidth = availableTextSpace / 2;
-        }
-        isAdjusting = false;
-      }
-    } );
+    // const availableTextSpace = maxWidth - TOGGLE_SWITCH_SIZE.width - SPACING * 2;
+    // let isAdjusting = false; // to prevent recursion that will exceed maximum call stack size
+    // this.boundsProperty.link( () => {
+    //   if ( !isAdjusting ) {
+    //     isAdjusting = true;
+    //
+    //     // Assume that neither label needs to be scaled.
+    //     firstLanguageText.maxWidth = null;
+    //     secondLanguageText.maxWidth = null;
+    //
+    //     // If there's not enough space for both full-size labels, give each Text label half of the available space.
+    //     if ( firstLanguageText.width + secondLanguageText.width > availableTextSpace ) {
+    //       firstLanguageText.maxWidth = availableTextSpace / 2;
+    //       secondLanguageText.maxWidth = availableTextSpace / 2;
+    //     }
+    //     isAdjusting = false;
+    //   }
+    // } );
   }
 }
 
