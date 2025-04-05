e78b2cab3460614f3aead3c5af23b88e2199a2bf
phet-dev <phetadmin@gmail.com>
2025-04-05 03:50:06 -0600

    update copyright dates from daily grunt work

--- js/common/view/NumberSuiteCommonSpeechSynthesisAnnouncer.ts
+++ js/common/view/NumberSuiteCommonSpeechSynthesisAnnouncer.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2022-2025, University of Colorado Boulder
 
 /**
  * An Announcer for speech synthesis that can be used with an UtteranceQueue. Used in Number Suite sims on screens that
