07d5f64d9b6f6df34bd522b0e6c106bbb8683f90
Marla Schulz <marla.schulz@colorado.edu>
2025-09-16 14:51:36 -0700

    Add no voice accessible paragraph, see https://github.com/phetsims/number-pairs/issues/200

--- js/common/view/NoVoiceWarningButton.ts
+++ js/common/view/NoVoiceWarningButton.ts
@@ -9,7 +9,7 @@
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
-import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import optionize, { combineOptions } from '../../../../phet-core/js/optionize.js';
 import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
 import Path from '../../../../scenery/js/nodes/Path.js';
 import Color from '../../../../scenery/js/util/Color.js';
@@ -19,8 +19,11 @@ import Tandem from '../../../../tandem/js/Tandem.js';
 import numberSuiteCommon from '../../numberSuiteCommon.js';
 import NumberSuiteCommonConstants from '../NumberSuiteCommonConstants.js';
 import NoVoiceWarningDialog from './NoVoiceWarningDialog.js';
+import { OopsDialogOptions } from '../../../../scenery-phet/js/OopsDialog.js';
 
-type SelfOptions = EmptySelfOptions;
+type SelfOptions = {
+  noVoiceWarningDialogOptions?: StrictOmit<OopsDialogOptions, 'tandem'>;
+};
 export type NoVoiceWarningButtonOptions = SelfOptions & StrictOmit<RectangularPushButtonOptions, 'content' | 'listener' | 'visibleProperty'>;
 
 // constants
@@ -29,10 +32,10 @@ const SIDE_LENGTH = NumberSuiteCommonConstants.BUTTON_LENGTH;
 class NoVoiceWarningButton extends RectangularPushButton {
 
   public constructor( hasVoiceProperty: TReadOnlyProperty<boolean>, providedOptions?: NoVoiceWarningButtonOptions ) {
+    const warningDialogOptions = combineOptions<OopsDialogOptions>( { tandem: providedOptions?.tandem?.createTandem( 'warningDialog' ) }, providedOptions?.noVoiceWarningDialogOptions );
+    const warningDialog = new NoVoiceWarningDialog( warningDialogOptions );
 
-    const warningDialog = new NoVoiceWarningDialog( { tandem: providedOptions?.tandem?.createTandem( 'warningDialog' ) } );
-
-    const options = optionize<NoVoiceWarningButtonOptions, SelfOptions, RectangularPushButtonOptions>()( {
+    const options = optionize<NoVoiceWarningButtonOptions, StrictOmit<SelfOptions, 'noVoiceWarningDialogOptions'>, RectangularPushButtonOptions>()( {
       content: new Path( exclamationTriangleSolidShape, {
         fill: new Color( 240, 79, 79 )
       } ),
