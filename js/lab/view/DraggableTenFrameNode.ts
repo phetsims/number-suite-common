2c27f31c8e55feb14cad1a92e54502f0e79f2e5c
phet-dev <phetadmin@gmail.com>
2026-03-20 10:29:39 -0600

    update copyright dates from daily grunt work

--- js/lab/view/DraggableTenFrameNode.ts
+++ js/lab/view/DraggableTenFrameNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2025, University of Colorado Boulder
+// Copyright 2019-2026, University of Colorado Boulder
 
 /**
  * A ten frame node that can be dragged around and hold countingObjects.
